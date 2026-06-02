ca1abb4dd96bf5fc3843df7ec85dac99de297ac6
plusultratablet1-tech <plusultratablet1@gmail.com>
2026-06-02 09:38:18 +0800

    Update ProjectGallery.tsx

--- components/sections/gallery/ProjectGallery.tsx
+++ components/sections/gallery/ProjectGallery.tsx
@@ -7,42 +7,49 @@ export default function ProjectGallery() {
       src: "/images/building-gallery-1.png",
       alt: "White residential building",
       className: "lg:col-start-1 lg:row-start-1 lg:row-span-2",
+      delayClass: "gallery-delay-1",
     },
     {
       id: 2,
       src: "/images/building-gallery-2.png",
       alt: "Modern building facade",
       className: "lg:col-start-2 lg:row-start-1",
+      delayClass: "gallery-delay-2",
     },
     {
       id: 3,
       src: "/images/building-gallery-3.png",
       alt: "Interior design lounge chair",
       className: "lg:col-start-3 lg:row-start-1 lg:row-span-2",
+      delayClass: "gallery-delay-3",
     },
     {
       id: 4,
       src: "/images/construction-crane.png",
       alt: "Construction site with cranes",
       className: "lg:col-start-1 lg:row-start-3 lg:row-span-2",
+      delayClass: "gallery-delay-4",
     },
     {
       id: 5,
       src: "/images/arch-bridge.png",
       alt: "Construction arch with crane",
       className: "lg:col-start-2 lg:row-start-2 lg:row-span-2",
+      delayClass: "gallery-delay-5",
     },
     {
       id: 6,
       src: "/images/building-construction.png",
       alt: "Construction crane closeup",
       className: "lg:col-start-2 lg:row-start-4",
+      delayClass: "gallery-delay-6",
     },
     {
       id: 7,
       src: "/images/office-building.png",
       alt: "Blue modern office building",
       className: "lg:col-start-3 lg:row-start-3 lg:row-span-2",
+      delayClass: "gallery-delay-7",
     },
   ];
 
@@ -53,16 +60,19 @@ export default function ProjectGallery() {
           {galleryItems.map((item) => (
             <div
               key={item.id}
-              className={`relative overflow-hidden rounded-[4px] bg-gray-100 ${item.className}`}
+              className={`gallery-card relative overflow-hidden rounded-[4px] bg-gray-100 ${item.className} ${item.delayClass}`}
             >
               <Image
                 src={item.src}
                 alt={item.alt}
                 fill
                 sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                className="object-cover"
+                className="gallery-image object-cover"
                 priority={item.id <= 3}
               />
+
+              {/* Shutter wipe overlay */}
+              <div className="gallery-wipe pointer-events-none absolute inset-0 z-10 bg-[#111827]" />
             </div>
           ))}
         </div>
